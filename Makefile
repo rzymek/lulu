b8f01a68a8b7c89de7e11f44f0dd2c5f52306aa7
rzymek <rzymek@gmail.com>
2017-02-09 23:25:11 +0100

    persist in localStorage (with debounce)

--- Makefile
+++ Makefile
@@ -1,12 +1,18 @@
-.ONESHELL:
-deploy: clean
+dev: build
+	yarn start
+
+build:
 	yarn build
 	rsync public/* build/
+	
+.ONESHELL:
+deploy: clean build
 	cd build
 	git init
 	git add :/
 	git commit -m 'deploy'
 	git remote add github git@github.com:rzymek/lulu.git
 	git push github HEAD:gh-pages --force
+
 clean:
 	rm -rf build/
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import * as React from 'react';
 import './App.css';
 import { parse } from "./parser";
+import * as debounce from "lodash.debounce";
 
-class App extends React.Component<{}, { text: string }> {
+class App extends React.Component<{}, { value: string }> {
+  updateState: (value: string) => void;
+  persist: (value: string) => void;
+
   constructor() {
     super();
     this.state = {
-      text: ''
+      value: ''
     }
+    this.updateState = debounce(value => this.setState({ value }), 100);
+    this.persist = debounce(value => localStorage.setItem("value", value), 1000);
+  }
+
+  handleChange(e) {
+    const {value} = e.target;
+    this.updateState(value);
+    this.persist(value);
   }
   render() {
     return <div>
-      <textarea style={{width:'48%'}} rows={30}
-        onChange={e => this.setState({ text: e.target.value })}>
+      <textarea style={{ width: '48%' }} rows={30}
+        onChange={this.handleChange.bind(this)}>
       </textarea>
-      <pre style={{width:'48%', float:'right'}}>
-        {JSON.stringify(parse(this.state.text), null, 2)}
+      <pre style={{ width: '48%', float: 'right' }}>
+        {JSON.stringify(parse(this.state.value), null, 2)}
       </pre>
     </div>
   }
