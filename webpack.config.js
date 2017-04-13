56347b23afb698b9a476c756cc38cb7097618697
rzymek <rzymek@gmail.com>
2017-04-14 00:20:50 +0200

    looking for the uglify flag that breaks firebase auth

--- webpack.config.js
+++ webpack.config.js
@@ -19,12 +19,12 @@ const config = {
             compress: {
                 sequences: false,  // join consecutive statemets with the “comma operator”
                 properties: false,  // optimize property access: a["foo"] → a.foo
-                dead_code: false,  // discard unreachable code
+                dead_code: true,  // discard unreachable code
                 drop_debugger: false,  // discard “debugger” statements
                 unsafe: false, // some unsafe optimizations (see below)
                 conditionals: false,  // optimize if-s and conditional expressions
                 comparisons: false,  // optimize comparisons
-                evaluate: false,  // evaluate constant expressions
+                evaluate: true,  // evaluate constant expressions
                 booleans: false,  // optimize boolean expressions
                 loops: false,  // optimize loops
                 unused: false,  // drop unused variables/functions
