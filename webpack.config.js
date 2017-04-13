31775229a09be3df8869e5a1c240a4d7a2bb8bf6
rzymek <rzymek@gmail.com>
2017-04-14 00:15:58 +0200

    disable uglify minification
    It interferes with firebase's auth causing:
    auth.js:149 Uncaught Error: Network Error
        at auth.js:149
        at i.fb (auth.js:47)
        at bn (auth.js:50)
        at vn (auth.js:50)
        at tn.ze (auth.js:49)
        at  (auth.js:43)
    
    To be exact UglifyJSPlugin.compress:true (not mangle:true) causes the problem

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,31 @@ const config = {
         extensions: ['.js', '.ts', '.tsx']
     },
     plugins: [
+        new UglifyJSPlugin({
+            compress: {
+                sequences: false,  // join consecutive statemets with the “comma operator”
+                properties: false,  // optimize property access: a["foo"] → a.foo
+                dead_code: false,  // discard unreachable code
+                drop_debugger: false,  // discard “debugger” statements
+                unsafe: false, // some unsafe optimizations (see below)
+                conditionals: false,  // optimize if-s and conditional expressions
+                comparisons: false,  // optimize comparisons
+                evaluate: false,  // evaluate constant expressions
+                booleans: false,  // optimize boolean expressions
+                loops: false,  // optimize loops
+                unused: false,  // drop unused variables/functions
+                hoist_funs: false,  // hoist function declarations
+                hoist_vars: false, // hoist variable declarations
+                if_return: false,  // optimize if-s followed by return/continue
+                join_vars: false,  // join var declarations
+                cascade: false,  // try to cascade `right` into `left` in sequences
+                side_effects: false,  // drop side-effect-free statements
+                warnings: false,  // warn about potentially dangerous optimizations/code
+                global_defs: {}
+            },
+            mangle: false,
+            sourceMap: true
+        }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         })
