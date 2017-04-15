a418bf5aab6975668ebbc75189ef1c7c2f279252
rzymek <rzymek@gmail.com>
2017-04-15 11:07:41 +0200

    Network Error

--- webpack.config.js
+++ webpack.config.js
@@ -19,12 +19,12 @@ const config = {
             compress: {
                 sequences: false,  // join consecutive statemets with the “comma operator”
                 properties: false,  // optimize property access: a["foo"] → a.foo
-                dead_code: true,  // discard unreachable code
+                dead_code: false,  // discard unreachable code
                 drop_debugger: false,  // discard “debugger” statements
                 unsafe: false, // some unsafe optimizations (see below)
                 conditionals: true,  // optimize if-s and conditional expressions
                 comparisons: false,  // optimize comparisons
-                evaluate: true,  // evaluate constant expressions
+                evaluate: false,  // evaluate constant expressions
                 booleans: false,  // optimize boolean expressions
                 loops: false,  // optimize loops
                 unused: true,  // drop unused variables/functions
