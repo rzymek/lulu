af4386569a07c420e7a68d4eefe9a1eb006d5b5c
rzymek <rzymek@gmail.com>
2017-04-15 11:24:14 +0200

    enable all UglifyJSPlugin.compress flags except unused

--- webpack.config.js
+++ webpack.config.js
@@ -17,27 +17,28 @@ const config = {
     plugins: [
         new UglifyJSPlugin({
             compress: {
-                sequences: false,  // join consecutive statemets with the “comma operator”
-                properties: false,  // optimize property access: a["foo"] → a.foo
-                dead_code: false,  // discard unreachable code
-                drop_debugger: false,  // discard “debugger” statements
-                unsafe: false, // some unsafe optimizations (see below)
-                conditionals: false,  // optimize if-s and conditional expressions
-                comparisons: false,  // optimize comparisons
-                evaluate: false,  // evaluate constant expressions
-                booleans: false,  // optimize boolean expressions
-                loops: false,  // optimize loops
-                unused: true,  // drop unused variables/functions
-                hoist_funs: false,  // hoist function declarations
-                hoist_vars: false, // hoist variable declarations
-                if_return: false,  // optimize if-s followed by return/continue
-                join_vars: false,  // join var declarations
-                cascade: false,  // try to cascade `right` into `left` in sequences
-                side_effects: false,  // drop side-effect-free statements
+                sequences: true,  // join consecutive statemets with the “comma operator”
+                properties: true,  // optimize property access: a["foo"] → a.foo
+                dead_code: true,  // discard unreachable code
+                drop_debugger: true,  // discard “debugger” statements
+                unsafe: true, // some unsafe optimizations (see below)
+                conditionals: true,  // optimize if-s and conditional expressions
+                comparisons: true,  // optimize comparisons
+                evaluate: true,  // evaluate constant expressions
+                booleans: true,  // optimize boolean expressions
+                loops: true,  // optimize loops
+                /* unused: true breaks firebase auth */
+                unused: false,  // drop unused variables/functions
+                hoist_funs: true,  // hoist function declarations
+                hoist_vars: true, // hoist variable declarations
+                if_return: true,  // optimize if-s followed by return/continue
+                join_vars: true,  // join var declarations
+                cascade: true,  // try to cascade `right` into `left` in sequences
+                side_effects: true,  // drop side-effect-free statements
                 warnings: false,  // warn about potentially dangerous optimizations/code
                 global_defs: {}
             },
-            mangle: false,
+            mangle: true,
             sourceMap: true
         }),
         new webpack.DefinePlugin({
