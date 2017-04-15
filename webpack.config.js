b149f1f000a5ab674ef351ac133eee0c02a3057b
rzymek <rzymek@gmail.com>
2017-04-15 11:24:55 +0200

    Enable all UglifyJSPlugin.compress flags except unused
    UglifyJSPlugin.compress.unused breaks firebase auth.
    The exact error is:
        auth.js:149 Uncaught Error: Network Error
            at auth.js:149
            at i.fb (auth.js:47)
            at bn (auth.js:50)
            at vn (auth.js:50)
            at tn.ze (auth.js:49)
            at  (auth.js:43)
    (During testing remeber to clear site data to trigger re-logging)

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,32 @@ const config = {
         extensions: ['.js', '.ts', '.tsx']
     },
     plugins: [
+        new UglifyJSPlugin({
+            compress: {
+                sequences: true,  // join consecutive statemets with the “comma operator”
+                properties: true,  // optimize property access: a["foo"] → a.foo
+                dead_code: true,  // discard unreachable code
+                drop_debugger: true,  // discard “debugger” statements
+                unsafe: true, // some unsafe optimizations (see below)
+                conditionals: true,  // optimize if-s and conditional expressions
+                comparisons: true,  // optimize comparisons
+                evaluate: true,  // evaluate constant expressions
+                booleans: true,  // optimize boolean expressions
+                loops: true,  // optimize loops
+                /* unused: true breaks firebase auth */
+                unused: false,  // drop unused variables/functions
+                hoist_funs: true,  // hoist function declarations
+                hoist_vars: true, // hoist variable declarations
+                if_return: true,  // optimize if-s followed by return/continue
+                join_vars: true,  // join var declarations
+                cascade: true,  // try to cascade `right` into `left` in sequences
+                side_effects: true,  // drop side-effect-free statements
+                warnings: false,  // warn about potentially dangerous optimizations/code
+                global_defs: {}
+            },
+            mangle: true,
+            sourceMap: true
+        }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         })
