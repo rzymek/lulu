dc8b19d67b122317a63b20c9af32712d668a8fa8
Krzysztof Rzymkowski <rzymek@gmail.com>
2022-04-16 21:46:17 +0200

    firebase init

---
+++ src/firebase.ts
@@ -0,0 +1,9 @@
+import { initializeApp } from 'firebase/app';
+
+initializeApp({
+  apiKey: 'AIzaSyCvK-h0vpW8cP92RSzOnxmk2B6PMUT0nNs',
+  authDomain: 'lulu-38151.firebaseapp.com',
+  databaseURL: 'https://lulu-38151.firebaseio.com',
+  messagingSenderId: '393438054532',
+  storageBucket: 'lulu-38151.appspot.com',
+});
